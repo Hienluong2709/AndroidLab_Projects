ce9acdc295e3a63011e445ef1da2b634810f275e
HienLuong <luongdo2709@gmail.com>
2025-07-27 16:26:49 +0700

    Add lab1

---
+++ Lab1_Project/app/src/main/java/com/example/lab1_project/MainActivity.java
@@ -0,0 +1,38 @@
+package com.example.lab1_project;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+    EditText edtA,edtB,edtKQ;
+    Button btnTong;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        edtA = findViewById(R.id.edtA);
+        edtB = findViewById(R.id.edtB);
+        edtKQ = findViewById(R.id.edtKQ);
+        btnTong = findViewById(R.id.btnTong);
+
+        btnTong.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                int a = Integer.parseInt(edtA.getText().toString());
+                int b = Integer.parseInt(edtB.getText().toString());
+                int c = a + b;
+                edtKQ.setText(String.valueOf(c));
+            }
+        });
+
+    }
+}
