e21de60192fe7f5968a154a927c1ddf6a4ab0c05
HienLuong <luongdo2709@gmail.com>
2025-08-02 22:51:07 +0700

    Add Lab9_Intent_service

---
+++ Lab9_Intent_Service_Project/app/src/main/java/com/example/lab9_intent_service_project/MainActivity.java
@@ -0,0 +1,37 @@
+package com.example.lab9_intent_service_project;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+    ImageButton btnPlay, btnPause;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        btnPlay = findViewById(R.id.btnPlay);
+        btnPause = findViewById(R.id.btnPause);
+
+        btnPlay.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, MyService.class);
+                startService(intent); // Bắt đầu phát nhạc
+            }
+        });
+
+        btnPause.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, MyService.class);
+                stopService(intent); // Dừng nhạc
+            }
+        });
+    }
+}
